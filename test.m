51af5d6a357a4702f5017d46e730e39f93f79f3d
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-09-25 09:47:25 +0200

    uprava tewst

--- test.m
+++ test.m
@@ -8,9 +8,48 @@ path_to_fundus='D:\Registrace_oci\Data\SLO\';
 name_fundus='Donalies_I_1088854_OD.png';
 name_AO='OD_20150909124229_X5.0T_Y-5.0_Z-30.0_DONALIES_Ingrid.png';
 
-
 AO=imread([path_to_AO name_AO]);
 fundus=imread([path_to_fundus name_fundus]);
+
+%%
+name=name_AO(19:end)
+for i=1:length(name)
+    switch name(i)
+        case 'X'
+            pom_num_start=i+1;
+            for m=pom_num_start:i+7
+                if name(m)=='_'
+                    pom_num_stop=m-1
+                    break
+                end
+            end
+            pom_X=name(pom_num_start:pom_num_stop);
+            X_posun=str2num(pom_X(1:end-1))
+            continue
+
+         case 'Y'
+            pom_num_start=i+1;
+            for m=pom_num_start:i+7
+                if name(m)=='_'
+                    pom_num_stop=m-1
+                    break
+                end
+            end
+            pom_Y=name(pom_num_start:pom_num_stop);
+            Y_posun=str2num(pom_Y(1:end-1))
+
+    end
+end
+
+
+%%
+figure
+subplot(1,2,1)
+imshow(fundus)
+subplot(1,2,2)
+imshow(AO)
+
+
 
 
 
