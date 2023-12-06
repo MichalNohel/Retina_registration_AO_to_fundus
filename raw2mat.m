77438cfde8e8564d60f6d1b8afad79a9857896ca
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-12-06 10:56:37 +0100

    zkouska registrace s elastixem

---
+++ raw2mat.m
@@ -0,0 +1,41 @@
+function [data,info] = raw2mat(path)
+
+% input is file path with .mhd
+
+fin=fopen([path]);
+name = (strjoin(cellstr(char(fread(fin)))));
+[~, ind2] = regexp(name,'D i m S i z e  =  ');
+s = strsplit(name(ind2+1:end),'  ');
+ind = regexp(s{1},' ');
+s{1}(ind)='';
+ind = regexp(s{2},' ');
+s{2}(ind)='';
+% ind = regexp(s{3},' ');
+% s{3}(ind)='';
+vel = [str2num(s{1}), str2num(s{2})];
+fclose('all');
+
+
+
+fin=fopen([ [path(1:end-4)] '.raw']);
+data=fread(fin,vel(1)*vel(2),'uint16=>uint16');
+
+data=reshape(data,vel(1),vel(2));
+data = permute(data,[2,1]);
+
+% info.size = vel;
+fclose('all')
+
+
+
+
+[~, ind2] = regexp(name,'E l e m e n t S p a c i n g  =  ');
+s = strsplit(name(ind2+1:end),'  ');
+ind = regexp(s{1},' ');
+s{1}(ind)='';
+ind = regexp(s{2},' ');
+s{2}(ind)='';
+% ind = regexp(s{3},' ');
+% s{3}(ind)='';
+res = [str2num(s{1}), str2num(s{2}), str2num(s{3})];
+info.resolution=res;
---
+++ test_elastix.m
@@ -0,0 +1,142 @@
+%% Retina registration of AO to fundus
+clear all
+close all
+clc
+path_to_AO='D:\Registrace_oci\Data\AO\';
+path_to_fundus='D:\Registrace_oci\Data\SLO\';
+%% Donalies Ingrid
+% name_fundus='Donalies_I_1088854_OD.png';
+% name_AO='OD_20150909124229_X5.0T_Y-5.0_Z-30.0_DONALIES_Ingrid.png';
+
+%% Kauerauf Cornelia
+name_AO='OD_20150909103446_X5.0N_Y5.0_Z50.0_KAUERAUF_Cornelia.png';
+name_fundus='Kauerauf_C_723617_0_OD.png'
+
+AO=imread([path_to_AO name_AO]);
+fundus=imread([path_to_fundus name_fundus]);
+
+%%
+name=name_AO(19:end)
+for i=1:length(name)
+    switch name(i)
+        case 'X'
+            pom_num_start=i+1;
+            for m=pom_num_start:i+7
+                if name(m)=='_'
+                    pom_num_stop=m-1
+                    break
+                end
+            end
+            pom_X=name(pom_num_start:pom_num_stop);
+            X_posun=str2num(pom_X(1:end-1))
+            continue
+
+         case 'Y'
+            pom_num_start=i+1;
+            for m=pom_num_start:i+7
+                if name(m)=='_'
+                    pom_num_stop=m-1
+                    break
+                end
+            end
+            pom_Y=name(pom_num_start:pom_num_stop);
+            Y_posun=str2num(pom_Y(1:end-1))
+
+    end
+end
+
+
+%%
+figure
+subplot(1,2,1)
+imshow(fundus)
+subplot(1,2,2)
+imshow(AO)
+
+%%
+velikost_obrazu=size(fundus);
+%%
+stred=[250,200];
+sirka_vyrezu=100;
+vyrez_fundus=fundus(stred(1)-sirka_vyrezu:stred(1)+sirka_vyrezu-1,stred(2)-sirka_vyrezu:stred(2)+sirka_vyrezu-1);
+imshow(vyrez_fundus)
+%%
+figure
+subplot(1,2,1)
+imshow(vyrez_fundus)
+subplot(1,2,2)
+imshow(AO)
+%% Elastix inicialization
+
+% [~,help] = system('D:\Registrace_oci\Data\elastix\elastix.exe --help')
+% [~,elastix_version] = system('D:\Registrace_oci\Data\elastix\elastix.exe --version')
+
+outputdir='D:\Registrace_oci\Data\TempFile';
+try
+    rmdir(outputdir,'s')
+catch err
+end
+mkdir(outputdir)
+%%
+fixed=vyrez_fundus;
+moving=AO;
+moving=imresize(moving,0.2);
+
+% fixed=AO;
+% moving=vyrez_fundus;
+% fixed=imresize(fixed,0.2);
+
+moving=medfilt2(moving,[7 7]);
+
+figure
+subplot(1,3,1)
+imshow(fixed,[])
+title('Refer')
+subplot(1,3,2)
+imshow(moving,[])
+title('Moving')
+%%
+
+imwrite(fixed,"D:\Registrace_oci\Data\TempFile\fixed.png")
+imwrite(moving,"D:\Registrace_oci\Data\TempFile\moving.png")
+
+
+
+%% Registratio - RIGID AND AFFINE
+
+% run ELASTIX
+
+file='D:\Registrace_oci\Data\parametricFiles\Parameters_Affine.txt';
+
+CMD=['D:\Registrace_oci\Data\elastix\elastix.exe ' '-f ' [outputdir '\fixed.png '] ' -m ' [outputdir '\moving.png '] ' -out ' outputdir ,' -p '  file];           
+system(CMD);
+
+
+%% read resulting RAW and save to variable - registered
+[registered]=raw2mat([outputdir '\result.0.mhd']);
+
+
+%% display
+figure
+subplot(1,3,1)
+imshow(fixed,[])
+title('Refer')
+subplot(1,3,2)
+imshow(moving,[])
+title('Moving')
+subplot(1,3,3)
+imshow(registered,[])
+title('Registered')
+
+%%
+% figure
+% subplot(1,2,1)
+% imshowpair(fixed,moving)
+% subplot(1,2,2)
+% imshowpair(fixed,registered)
+
+
+
+
+
+
